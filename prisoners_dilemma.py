b2e789f9d63dcf7fd9a6bf0b5405040fd4e22e1a
Rod Myers <r_myers@easdpa.org>
2016-03-02 05:42:46 -0500

    Improvement to my WOW statement
    Made yet another change to show how excited I am by GitHub

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 ''' 
-WOW, I've just made a change to my testing branch!
+WOW, I've just made a change to my vey cool testing branch!
 PrisonerDilemma.py allows hard-coding different strategies
 for the Iterative Prisoners Dilemma, the canonical game of game-theory.
 Each strategy plays 100 to 200 rounds against each other strategy.
